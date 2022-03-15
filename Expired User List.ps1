fb609d48a34e2346afed5613020c16a9e5cd64be
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-15 13:07:28 -0700

    Add files via upload

---
+++ Expired User List.ps1
@@ -0,0 +1,3 @@
+﻿#Display Expired User Accounts
+$ThisDomain = "LON-DC1"
+Search-ADAccount -Server $ThisDomain -Credential $Creds -AccountExpired -UsersOnly -ResultPageSize 2000 -resultSetSize $null| Select-Object Name, SamAccountName, DistinguishedName, AccountExpirationDate
