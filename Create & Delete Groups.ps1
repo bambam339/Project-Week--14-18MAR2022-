e9aef3fdb38b55af9e7b2fef80036694adf36726
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-14 20:25:08 -0700

    Add files via upload

---
+++ Create & Delete Groups.ps1
@@ -0,0 +1,23 @@
+﻿#Create a Global Security Group with Accidental Deletion Protection
+$NewGroup = "NewGroup"
+New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:$NewGroup -Path:"DC=Adatum,DC=com" -SamAccountName:$NewGroup -Server:"LON-DC1.Adatum.com"
+Set-ADObject -Identity:"CN=NewGroup,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"LON-DC1.Adatum.com"
+
+#Delete a Global Security Group with Accidental Deletion Protection
+$DeleteGroup = "NewGroup"
+Set-ADObject -Identity:"CN=$DeleteGroup,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$false -Server:"LON-DC1.Adatum.com"
+Remove-ADObject -Confirm:$false -Identity:"CN=$DeleteGroup,DC=Adatum,DC=com" -Server:"LON-DC1.Adatum.com"
+
+#Create Multiple Groups with Accidental Protection Deletion
+$NewGroups = @('A1','A2','A3')
+foreach ($Group in $NewGroups)
+{
+New-ADGroup -GroupCategory:"Security" -GroupScope:"Global" -Name:$Group -Path:"DC=Adatum,DC=com" -SamAccountName:$Group -Server:"LON-DC1.Adatum.com"
+Set-ADObject -Identity:"CN=$Group,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"LON-DC1.Adatum.com"}
+
+#Delete Multiple Groups with Accidental Protection Deletion
+$DeleteGroups = @('A1','A2','A3')
+foreach ($Group in $DeleteGroups)
+{
+Set-ADObject -Identity:"CN=$Group,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$false -Server:"LON-DC1.Adatum.com"
+Remove-ADObject -Confirm:$false -Identity:"CN=$Group,DC=Adatum,DC=com" -Server:"LON-DC1.Adatum.com"}
--- Create & Delete Multiple OUs.ps1
+++ Create & Delete Multiple OUs.ps1
@@ -1,41 +1,13 @@
-﻿# This script is used for creating bulk organizational units.
-
-# Import active directory module for running AD cmdlets
-Import-Module activedirectory
-
-#Store the data from the CSV in the $ADOU variable. 
-$Parentname = @('Atlanta','Boston')
-$Parentpath = "DC=Adatum,DC=com"
-
-#Loop through each row containing user details in the CSV file
+﻿#Create Multiple OUs with Accidental Protection Deletion
+$ADou = @('A1','A2','A3')
 foreach ($ou in $ADou)
 {
-#Read data from each field in each row and assign the data to a variable as below
-$name = $ou.name
-$path = $ou.path
-
-#Account will be created in the OU provided by the $OU variable read from the CSV file
-New-ADOrganizationalUnit `
--Name $name `
--path $path `
-
-}
-
-$ADou = @('Test1','A2','A3')
-$path = "DC=Adatum,DC=com"
-foreach ($ou in $ADou)
-New-ADOrganizationalUnit -Name:$ou -Path:"DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"LON-DC1.Adatum.com"
-Set-ADObject -Identity:"OU=$ou,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"LON-DC1.Adatum.com"
-
-#Create an OU with Accidental Deletion Protection
-$ADou = "A1"
-New-ADOrganizationalUnit -Name:$ADou -Path:"DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"LON-DC1.Adatum.com"
-Set-ADObject -Identity:"OU=$ADou,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"LON-DC1.Adatum.com"
-
-
-
+New-ADOrganizationalUnit -Name:$ou -Path:"DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"LON-DC1.Adatum.com" |
+Set-ADObject -Identity:"OU=$ou,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$true -Server:"LON-DC1.Adatum.com"}
 
 #Delete an OU with Accidental Deletion Protection
-$OU= 'London'
+$ADou = @('A1','A2','A3')
+foreach ($ou in $ADou)
+{
 Set-ADObject -Identity:"OU=$OU,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$false -Server:"LON-DC1.Adatum.com"
-Remove-ADObject -Confirm:$false -Identity:"OU=$OU,DC=Adatum,DC=com" -Server:"LON-DC1.Adatum.com"
+Remove-ADObject -Confirm:$false -Identity:"OU=$OU,DC=Adatum,DC=com" -Server:"LON-DC1.Adatum.com"}
