97318d00b94237faf73074a19267a3fe685ed1e3
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-15 16:28:13 -0700

    Add files via upload

---
+++ Remote Host Disk Size & Freespace.ps1
@@ -0,0 +1,8 @@
+﻿Get-WmiObject -Class Win32_LogicalDisk -ComputerName LON-CL1,LON-DC1,LON-SVR1 -Filter "DeviceID='C:'" |
+Select-Object SystemName,
+              @{n='FreeSpace(GB)';e={$PSItem.FreeSpace/1GB}},
+              @{n='Disk Size(GB)';e={$PSItem.Size/1GB}}
+
+
+Get-WmiObject  -Class Win32_LogicalDisk | GM
+
