05db59918fadb80184f73532fc52eb49a8ac1a4d
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-15 13:00:17 -0700

    Add files via upload

---
+++ Create & Delete Bulk Users.ps1
@@ -0,0 +1,14 @@
+﻿#Create Multiple New Users 
+$ADUsers = @("A B", "C D", "E F")
+$Dept = "IT"
+foreach ($User in $NewUsers)
+{
+New-ADUser -Company:"Adatum" -Department:$Dept -Name:$User} 
+
+#Delete Multiple Users with Accidental Deletion Protection
+$DelUsers = @("A B", "C D", "E F")
+foreach ($User in $DelUsers)
+{
+Remove-ADObject -Confirm:$false -Identity:"CN=$User,CN=Users,DC=Adatum,DC=com" -Server:"LON-DC1.Adatum.com"
+Set-ADObject -Identity:"CN=$User,CN=Users,DC=Adatum,DC=com" -ProtectedFromAccidentalDeletion:$false -Server:"LON-DC1.Adatum.com"
+}
