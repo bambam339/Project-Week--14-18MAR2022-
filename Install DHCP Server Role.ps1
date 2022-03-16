0093d2565610a7e91774bae2ebd5ff5e5ae49ab5
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-16 10:13:33 -0700

    Add files via upload

---
+++ Install DHCP Server Role.ps1
@@ -0,0 +1,12 @@
+﻿#1.Install the DHCP server role
+Add-WindowsFeature DHCP
+
+#Or Install the DHCP management console with DHCP server role
+Add-WindowsFeature –IncludeManagementTools DHCP 
+
+#2. Post-Installation
+#Create the security groups
+Netsh DHCP Add SecurityGroups
+
+#Restart DHCP service
+Restart-Service DHCPServer 
