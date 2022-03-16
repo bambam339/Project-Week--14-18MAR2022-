ff1ae652858fa52f4b14276a92d5dfe0a7b23dcf
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-16 14:16:58 -0700

    Add files via upload

---
+++ Install VPN Server Role.ps1
@@ -0,0 +1,14 @@
+﻿#Install Remote Access as a RAS Gateway VPN Server
+Install-WindowsFeature DirectAccess-VPN -IncludeManagementTools
+
+#Retrieve Global VPN Profiles
+Get-VPNConnection -AllUserConnection
+
+#Retrieve all local VPN profiles
+Get-VPNConnection 
+
+#Enables split tunneling for the A. Datum VPN profile
+Set-VpnConnection -name "A. Datum VPN" -AllUserConnection -SplitTunneling $true 
+
+#Add VPN Connection
+Add-VpnConnection -Name "Test1" -ServerAddress "10.1.1.1" -PassThru
