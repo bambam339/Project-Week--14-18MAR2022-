4c95e069ac71eb23b1033b2e3aa6d8534a1b04db
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-16 13:59:52 -0700

    Add files via upload

---
+++ Update DirectAccess Clients.ps1
@@ -0,0 +1,11 @@
+﻿#Update DirectAccess clients
+gpupdate
+
+#Display Name Resolution Policy Table (NRPT) entries for DirectAccess
+Get-DnsClientNrptPolicy
+
+#Display network connectivity status indicator settings
+Get-NCSIPolicyConfiguration 
+
+#Check if client can reach the network location server URL (ConnectedLocally)
+Get-DAConnectionStatus
