62480e64164cfb6bcb6c928bded2a18b3c69839a
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-16 10:44:51 -0700

    Add files via upload

---
+++ Install DNS Server Role.ps1
@@ -0,0 +1,26 @@
+﻿#Install DNS Server Role
+Install-WindowsFeature DNS -IncludeManagementTools
+
+#Configure DNS Forwarding
+$IPAddress = 131.170.0.100
+Add-DnsServerForwarder -IPAddress $IpAddress
+
+#Check for current forwarders
+Get-DnsServerForwarder
+
+#Remove DNS Forwarder
+$IPAddress = '131.170.0.100'
+Remove-DnsServerForwarder -IPAddress $IPAddress
+
+#Configure DNS Conditional Forwarding
+$ZoneName = "Adatum.com"
+$ForwarderAdd = '172.16.0.10'
+Add-DnsServerConditionalForwarderZone -Name $ZoneName -MasterServers $ForwarderAdd 
+
+#Check Current Zones
+Get-DnsServerZone 
+
+#Remove Conditional Forwarder
+$ZoneName = "Adatum.com"
+Remove-DnsServerZone -Name NameOfZoneHere
+
