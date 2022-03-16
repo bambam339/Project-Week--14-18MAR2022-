02b9a250d8b1c7b7d459b22e7df2af529a6194ee
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-16 15:33:00 -0700

    Add files via upload

---
+++ Configure DFS Namespaces and Replication.ps1
@@ -0,0 +1,41 @@
+﻿#show the File Server and DFS components
+get-windowsfeatures -name fs
+
+#Install the DFS Replication role service
+Install-WindowsFeature -Name FS-DFS-Namespace,FS-DFS-Replication –IncludeManagementTools
+
+#Create a new namespace (domain-based namespace on LON-SVR1 named Research)
+$Server = LON-SVR1
+$Path = "\\Adatum.com\BranchDocs"
+$Name = BranchDocs
+New-DfsnRoot -TargetPath $Path -Asjob -Type DomainV2
+
+#Create a new folder and folder target
+New-DfsnFolder -Path "\\Contoso\AccountingResources\LegacySoftware" -TargetPath "\\Contoso-FS\AccountingLegacy" -EnableTargetFailback $True -Description "Folder for legacy software."
+
+#Install DFS Replication Module
+Install-Module DFSR
+
+#Create a new folder target for replication
+New-DfsReplicatedFolder -GroupName "RG22" -FolderName "RF26"
+
+
+GroupName              : RG22
+FolderName             : RF26
+DomainName             : corp.contoso.com
+Identifier             : 434ca205-388d-4f24-a5b0-7ff875d433e0
+Description            :
+FileNameToExclude      : {~*, *.bak, *.tmp}
+DirectoryNameToExclude : {}
+DfsnPath               :
+IsDfsnPathPublished    : False
+State                  : Normal
+
+#Create a new replication group
+New-DfsReplicationGroup -GroupName "RG01"
+
+GroupName              : RG01
+DomainName             : corp.contoso.com
+Identifier             : 81251362-e30f-4c1e-b6b0-23906c1ebdd7
+Description            : 
+State                  : Normal
