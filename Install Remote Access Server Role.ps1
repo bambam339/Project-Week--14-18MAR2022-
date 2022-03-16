3354def978fecc4759fdebae8701311a61602ccb
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-16 13:52:31 -0700

    Add files via upload

--- Install Remote Access Server Role.ps1
+++ Install Remote Access Server Role.ps1
@@ -1,2 +1,8 @@
 ﻿#Install Remote Access Server Role
 Install-WindowsFeature RemoteAccess -IncludeManagementTools
+
+#Restart Computer
+Restart-Computer
+
+#Install Remote Access in Powershell
+Install-WindowsFeature RSAT-RemoteAccess-PowerShell
