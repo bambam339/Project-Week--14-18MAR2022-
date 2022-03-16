f250f40e1cfff493ef8c7e0041fad8d451802092
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-16 09:05:30 -0700

    Add files via upload

---
+++ Release and renew DHCP leases on Adapters.ps1
@@ -0,0 +1,19 @@
+﻿#Renew DHCP Lease
+ipconfig /renew
+
+#renew the lease on a specific connection
+ipconfig /renew "Wireless Network Connection 4"
+
+#renew the lease on a remote computer
+$ComputerName = "LON-DC1"
+GetWmiObject Win32_NetworkAdapterConfiguration –Computer $ComputerName
+
+#Renew a Specific Adapter
+$ComputerName = "LON-DC1"
+$description = "Linksys WirelessG USB"
+$adapter = GetWmiObject Win32_NetworkAdapterConfiguration –Computer $ComputerName |
+WhereObject { $_.Description –match $description}
+$adapter.RenewDHCPLease()
+
+#Check Ipconﬁg on the remote computer
+InvokeRemoteExpression \\$ComputerName { ipconfig /renew 
