1b310b428e039dacea6f254d4f457980b7c9dd58
Crisanta E. Jaggers <98923223+bambam339@users.noreply.github.com>
2022-03-17 14:47:56 -0700

    Add files via upload

---
+++ 1.0.2 Configure IPv4.ps1
@@ -0,0 +1,17 @@
+﻿
+#20741B - Networking with Windows Server 2016
+#1.Demo1 Configure IPv4
+#Configuring IPv4 by using the user interface
+
+#Check IP Address
+Get-NetIPAddress -InterfaceAlias “London_Network”
+
+#Remove IP Address
+Remove-NetIPAddress -InterfaceAlias “London_Network” -IPAddress 172.16.0.111
+
+#Add New IPAddress
+New-NetIPAddress -InterfaceAlias “London_Network” -IPAddress 172.16.0.11 -PrefixLength 24 
+
+#Enable DHCP for the adapter Local Area Connection
+Set NetIPInterface –InterfaceAlias "Local Area Connection" –Dhcp Enabled 
+Restart NetAdapter –Name "Local Area Connection" 
---
+++ 1.0.3 Troubleshoot IPv4.ps1
@@ -0,0 +1,13 @@
+﻿
+#Demonstration: Troubleshooting IPv4
+#Task 1: Using Get-NetIPAddress and ipconfig
+Get-NetIPAddress -InterfaceAlias “London_Network” 
+ipconfig 
+
+#Task 2: Using Test-NetConnection and ping
+Test-NetConnection 172.16.0.1 
+Ping 172.16.0.1 
+
+#Using Test-NetConnection –TraceRoute and tracert
+Test-NetConnection -TraceRoute 172.16.18.20 
+Tracert 172.16.18.20 
